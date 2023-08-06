5abf9c2d4e1a284e7e227bd0b92c70bc6157c22d
Sravanti Chinta <91924223+ShravesChinta@users.noreply.github.com>
2023-08-05 22:25:02 -0400

    Create Two-sum.cs

---
+++ Arrays/Two-sum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+    Dictionary<int, int> map=new Dictionary<int, int>();
+        int[] result=new int[2];
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(map.ContainsKey(target-nums[i]))
+            {
+                result[0]=map[target-nums[i]];
+                result[1]=i;
+                break;
+            }
+
+            map[nums[i]]=i;
+        }
+
+        return result;
+    }
+}
